e1d96fe8e329f27c733deeeb0766aab60d2cff38
cvwj <christian@adamatics.com>
2024-03-13 07:35:28 +0100

    Bump to 3.2.3 and java 21

--- src/main/java/dk/lundogbendsen/springbootcourse/urlshortener/model/User.java
+++ src/main/java/dk/lundogbendsen/springbootcourse/urlshortener/model/User.java
@@ -1,9 +1,9 @@
 package dk.lundogbendsen.springbootcourse.urlshortener.model;
 
-import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
 
 @Getter
 @Setter
